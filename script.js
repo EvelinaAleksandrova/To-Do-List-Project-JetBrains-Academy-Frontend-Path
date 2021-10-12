60201c916a5444f88cc49d7ac5c856a9a1102a5f
evelina <evety.1dtvd@gmail.com>
2021-10-12 16:06:33 +0300

    When checkbox is checked -> task name would have line-through. Add media Queries for responsive design.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let counterID = 3;
 function addTaskToUL() {
+    counterID++;
     let content = document.getElementById("input-task").value;
     let newLi = document.createElement("li");
     newLi.innerHTML = `<li>
@@ -6,6 +8,9 @@ function addTaskToUL() {
                 <span class="task">${content}</span>
                 <button class="delete-btn" onclick="deleteTask(this)">X</button>
             </li>`;
+    if(content === ""){
+        return;
+    }
     document.getElementById('task-list').appendChild(newLi);
     document.getElementById("input-task").value = "";
     console.log("Add task --" + content);
@@ -15,5 +20,10 @@ function addTaskToUL() {
 function deleteTask(obj) {
     console.log("Delete task");
     obj.parentNode.remove();
+}
+
+function line(){
+
+
 }
 
